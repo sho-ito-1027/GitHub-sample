9dc3d390ac93b8cfcd0568e2f870f07794f36540
aryzae <a.aryzae@gmail.com>
2024-09-17 10:19:29 +0900

    feat: add project

---
+++ GitHubSampleWithTCA/GitHubSampleWithTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubSampleWithTCA
+//
+//  Created by aryzae on 2024/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubSampleWithTCA/GitHubSampleWithTCA/GitHubSampleWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSampleWithTCAApp.swift
+//  GitHubSampleWithTCA
+//
+//  Created by aryzae on 2024/09/17.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSampleWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
